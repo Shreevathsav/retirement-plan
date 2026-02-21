0f04ceb9d847ebfde820cffeb6524ebc2128960f
Shreevathsav <58297203+Shreevathsav@users.noreply.github.com>
2026-02-21 12:25:42 +0530

    project init

---
+++ retirement-plan/src/main/java/com/retirement/plan/RetirementPlanApplication.java
@@ -0,0 +1,13 @@
+package com.retirement.plan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetirementPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetirementPlanApplication.class, args);
+	}
+
+}
